7312ec6c785c97b5a1204988eb6f7720cf74f2b7
Mijail <mijail.casoni@outlook.cl>
2025-07-10 01:10:08 -0400

    agregar comandos de tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-cremaCafe text-cafeTostado`}>
         {children}
       </body>
     </html>
